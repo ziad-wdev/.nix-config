e8ea5e8e69e235f3140aaf0a42c13ef7d6a98f2e
ziad-wdev <ziadahmed2371@gmail.com>
2026-06-04 00:43:27 +0300

    Add more qylock themes
    This commit introduces a comprehensive list of qylock themes, allowing users to select from a wider variety of
    aesthetically pleasing options for their login manager. The previously hardcoded "pixel-hollowknight" theme is now
    configurable through the `themes` attribute set.

--- hosts/default/sddm.nix
+++ hosts/default/sddm.nix
@@ -19,9 +19,50 @@
 
   environment.systemPackages = [ pkgs.bibata-cursors ];
 
-  programs.qylock = {
-      enable = true;
-      theme = "pixel-hollowknight";
-      sddmTheme = "pixel-hollowknight";
+  programs.qylock = let
+    themes = {
+      "Genshin Impact" = "Genshin";
+      "Reverse: 1999 - I" = "R1999_1";
+      "Reverse: 1999 - II" = "R1999_2";
+      "Clockwork" = "clockwork";
+      "Dog Samurai" = "dog-samurai";
+      "Enfield" = "enfield";
+      "Field" = "field";
+      "Forest" = "forest";
+      "Girl · Coffee" = "girl-coffee";
+      "Girl · Pillow" = "girl-pillow";
+      "The Last of Us" = "last-of-us";
+      "Man · Bicycle" = "man-bicycle";
+      "Material You" = "material-you";
+      "Minecraft" = "minecraft";
+      "NieR: Automata" = "nier-automata";
+      "Ninja Gaiden" = "ninja_gaiden";
+      "Nothing" = "nothing";
+      "osu!" = "osu";
+      "osu! mania" = "osumania";
+      "Pixel · Coffee" = "pixel-coffee";
+      "Pixel · Cyberpunk" = "pixel-cyberpunk";
+      "Pixel · Dusk City" = "pixel-dusk-city";
+      "Pixel · Emerald" = "pixel-emerald";
+      "Pixel · Hollow Knight" = "pixel-hollowknight";
+      "Pixel · Munchlax" = "pixel-munchlax";
+      "Pixel · Night City" = "pixel-night-city";
+      "Pixel · Rainy Room" = "pixel-rainyroom";
+      "Pixel · Sakura" = "pixel-sakura";
+      "Pixel · Skyscrapers" = "pixel-skyscrapers";
+      "Pixel · Waterfall" = "pixel-waterfall";
+      "Honkai: Star Rail" = "star-rail";
+      "Sword" = "sword";
+      "Terraria" = "terraria";
+      "Windows 7" = "windows_7";
+      "Winter" = "winter";
+      "Women · Umbrella" = "women-umbrella";
+      "Wuthering Waves" = "wuwa";
+    };
+    theme = themes."Dog Samurai";
+  in {
+    enable = true;
+    theme = theme;
+    sddmTheme = theme;
   };
 }
