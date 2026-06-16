4dc1bd26aa656a6ddf2ea7f83d8ec73832723929
ziad-wdev <ziadahmed2371@gmail.com>
2026-06-16 04:26:19 +0300

    changed sddm compositor and theme

--- modules/apps/sddm.nix
+++ modules/apps/sddm.nix
@@ -9,7 +9,7 @@
     enable = true;
     wayland = {
       enable = true;
-      compositor = "kwin";
+      compositor = "hyprland";
     };
 
     settings = {
@@ -61,7 +61,7 @@
         "women-umbrella" # 35
         "wuwa" # 36
       ];
-      theme = builtins.elemAt themes 7;
+      theme = builtins.elemAt themes 23;
     in
     {
       enable = true;
